52b5b9e5c9ca6ce90a0c81a71af8082f424992da
Yuriy Vorobyev <42265532+Yurivo@users.noreply.github.com>
2024-12-09 16:00:51 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name='John'
+print( 'Name:' + name)
+age = 26
+new_age = age + 1
+print('Age:' + str(age))
+print('New Age:' + str(new_age))
+is_student = True
+print('IS Student:' + str(is_student))
